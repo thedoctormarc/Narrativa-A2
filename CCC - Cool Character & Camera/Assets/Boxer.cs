3e90d4ac6d2525e6605516decd0a67426db251d8
thedoctormarc <36292102+thedoctormarc@users.noreply.github.com>
2021-03-23 12:20:14 +0100

    Character animator done

---
+++ CCC - Cool Character & Camera/Assets/Boxer.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boxer : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField]
+    GameObject targetLook;
+
+    [SerializeField]
+    [Range(1f,3f)]
+    float moveSpeed = 1.5f;
+
+    Vector2 movement;
+
+    CharacterController cc;
+
+    Animator anim;
+
+    bool punch;
+
+    bool dodgeR, dodgeL;
+
+    void Start()
+    {
+        movement = new Vector2();
+        movement = Vector2.zero;
+        cc = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.LookAt(targetLook.transform);
+
+        // Movement
+        if (Input.GetKey(KeyCode.W))
+            movement.y = 1f;
+
+        if (Input.GetKey(KeyCode.S))
+            movement.y = -1f;
+
+        if (Input.GetKey(KeyCode.D))
+            movement.x = 1f;
+
+        if (Input.GetKey(KeyCode.A))
+            movement.x = -1f;
+
+        // Punch
+        punch = false;
+        anim.SetBool("punch", false);
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if(anim.GetBool("idle"))
+            {
+                punch = true;
+                anim.SetBool("idle", false);
+                anim.SetBool("punch", true);
+            }
+     
+        }
+
+        // Dodge
+
+        dodgeL = false;
+        anim.SetBool("dodge left", false);
+
+        dodgeR = false;
+        anim.SetBool("dodge right", false);
+
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            if (anim.GetBool("idle"))
+            {
+                punch = true;
+                anim.SetBool("idle", false);
+                anim.SetBool("dodge left", true);
+            }
+
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            if (anim.GetBool("idle"))
+            {
+                punch = true;
+                anim.SetBool("idle", false);
+                anim.SetBool("dodge right", true);
+            }
+
+        }
+
+        // Resolve Movement
+        Vector3 delta_fwd = Vector3.forward * movement.y * Time.deltaTime * moveSpeed;
+        cc.Move(delta_fwd);
+
+        Vector3 delta_side = Vector3.right * movement.x * Time.deltaTime * moveSpeed;
+        cc.Move(delta_side);
+
+        if (movement.magnitude > 0f)
+            anim.SetBool("idle", false);
+        else
+            anim.SetBool("idle", true);
+
+
+        movement = Vector2.zero;
+    }
+
+
+
+}
